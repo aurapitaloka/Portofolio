f98ef2acae29728308a129831fdf549416de45ad
aurapitaloka <aurapita131@gmail.com>
2026-04-14 22:04:35 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  base: '/Portofolio/',
-})
+export default defineConfig(({ mode }) => ({
+  base: mode === 'gh' ? '/Portofolio/' : '/',
+}))
